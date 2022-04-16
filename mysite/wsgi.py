a2caad4258dc371172f78384cd6a1425c702ffb1
JaCait <filip.tru.26@gmail.com>
2022-04-16 15:55:06 +0200

    changes v3

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,11 +10,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-application = get_wsgi_application()
-
+# Add your project directory to the sys.path
 settings_path = '/home/JaCait/jacait.pythonanywhere.com'
 sys.path.insert(0, settings_path)
+
+# Set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+# Set the 'application' variable to the Django wsgi app
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
