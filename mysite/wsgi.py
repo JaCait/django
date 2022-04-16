ac9848017b7248ec06b2c481d54f54b6072bcecb
JaCait <filip.tru.26@gmail.com>
2022-04-16 15:11:07 +0200

    change

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+settings_path = '/home/JaCait/jacait.pythonanywhere.com'
+sys.path.insert(0, settings_path)
